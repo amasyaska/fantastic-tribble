520cd76f6b71d45600d4692422096f83193a4f97
GineTik <densheva18072003@gmail.com>
2024-11-15 20:09:05 +0200

    add: dark theme

--- frontend/src/components/layouts/home/header/HomeHeader.tsx
+++ frontend/src/components/layouts/home/header/HomeHeader.tsx
@@ -1,5 +1,33 @@
+import { Button } from '@components/ui/button/Button'
+import { useCallback, useState } from 'react'
+import { GrMoon, GrSun } from 'react-icons/gr'
+
 type HomeHeaderProps = {}
 
 export const HomeHeader = ({}: HomeHeaderProps) => {
-	return <div>HomeHeader</div>
+	const changeTheme = useCallback(() => {
+		if (!document) return
+
+		document.body.classList.toggle('light')
+		document.body.classList.toggle('dark')
+	}, [document])
+
+	const [theme, setTheme] = useState('dark')
+	const handleThemeSwitcher = () => {
+		const newTheme = theme === 'dark' ? 'light' : 'dark'
+		setTheme(newTheme)
+		document.documentElement.classList.toggle('dark', newTheme === 'dark')
+	}
+
+	return (
+		<div className='flex p-3'>
+			HomeHeader
+			<div className='ml-auto'>
+				<Button variant='outline' size='icon' onClick={handleThemeSwitcher}>
+					<GrMoon className='hidden dark:block' />
+					<GrSun className='block dark:hidden' />
+				</Button>
+			</div>
+		</div>
+	)
 }
