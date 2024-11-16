8035e203813d71ea3a176f54043067c5df77d779
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-16 15:16:20 +0200

    created CustomUser model

--- src/backend/djangowebserver/accounts/models.py
+++ src/backend/djangowebserver/accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy
+from .managers import CustomUserManager
 
-# Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=127)
+    last_name = models.CharField(max_length=127)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified=models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=128)
+    avatar = models.CharField(max_length=256)
+
+    USERNAME_FIELDS = 'username'
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
