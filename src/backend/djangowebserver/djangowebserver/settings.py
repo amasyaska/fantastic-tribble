6f393b24f2ae3c192ebc0004962ec64e9da22397
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-16 13:58:48 +0200

    added corsheaders, accounts and social_accounts to installed apps added corsheaders middleware

--- src/backend/djangowebserver/djangowebserver/settings.py
+++ src/backend/djangowebserver/djangowebserver/settings.py
@@ -45,13 +45,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    
+    'corsheaders',
 
     'api',
+    'accounts',
+    'social_accounts',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
