812d15526d21039087689d1bbba8395b6ef2173e
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 00:30:48 +0200

    added cors allowed hosts for frontend

--- src/backend/djangowebserver/djangowebserver/settings.py
+++ src/backend/djangowebserver/djangowebserver/settings.py
@@ -154,3 +154,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
+    'http://127.0.0.1',
+    'http://0.0.0.0',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
