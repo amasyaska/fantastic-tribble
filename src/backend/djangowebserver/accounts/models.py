39f80748b7d261eca55f2126082ba51f48b0f3b0
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-16 15:38:29 +0200

    removed typo

--- src/backend/djangowebserver/accounts/models.py
+++ src/backend/djangowebserver/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy
+
 from .managers import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -18,7 +19,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(max_length=128)
     avatar = models.CharField(max_length=256)
 
-    USERNAME_FIELDS = 'username'
+    USERNAME_FIELD = 'username'
 
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
