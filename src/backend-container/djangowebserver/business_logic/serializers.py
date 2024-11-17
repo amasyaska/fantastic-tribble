5658f237c287ba1782f93cb852859783c4e8731d
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 15:36:18 +0200

    implemented Company serializer

---
+++ src/backend-container/djangowebserver/business_logic/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Company
+
+class CompanyCreateSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CustomUser
+        fields = ('name', 'description', 'creator_id')
+
+    def create(self, validated_data):
+        user = Company.objects.create(
+            name = validated_data['name'],
+            description = validated_data['description'],
+            creator_id = validated_data['creator_id']
+        )
+        return user
