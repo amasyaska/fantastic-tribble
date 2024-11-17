99d6c8cb94cc753e3fa7ac3e062d7992b43c6e4f
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 21:06:52 +0200

    created ProjectCreateSerializer

--- src/backend-container/djangowebserver/business_logic/serializers.py
+++ src/backend-container/djangowebserver/business_logic/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Company, CompanyCustomUser
+from .models import Company, CompanyCustomUser, Project
 
 class CompanyCreateSerializer(serializers.ModelSerializer):
     
@@ -26,6 +26,20 @@ class CompanyMembersSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         obj = CompanyCustomUser.objects.create(
             user = validated_data['user'],
+            company = validated_data['company']
+        )
+        return obj
+    
+class ProjectCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ('name', 'description', 'company')
+
+    def create(self, validated_data):
+        obj = Project.objects.create(
+            name = validated_data['name'],
+            description = validated_data['description'],
             company = validated_data['company']
         )
         return obj
