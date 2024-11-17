9edeae1110a4f9401bdebdda5ab6318d3fe1c96d
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 21:40:28 +0200

    implemented my_companies/ endpoint

--- src/backend-container/djangowebserver/business_logic/urls.py
+++ src/backend-container/djangowebserver/business_logic/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import CompanyAPIView, CompanyMembersAPIView, ProjectAPIView
+from .views import CompanyAPIView, CompanyMembersAPIView, ProjectAPIView, CustomUserCompaniesAPIView
 
 urlpatterns = [
     path('company/', CompanyAPIView.as_view(http_method_names=['post', 'options'])),
     path('company/<int:id>', CompanyAPIView.as_view(http_method_names=['get', 'put', 'delete'])),
     path('company/<int:company_id>/members/', CompanyMembersAPIView.as_view(http_method_names=['get', 'post', 'put'])),
     path('company/<int:company_id>/members/<int:member_id>', CompanyMembersAPIView.as_view(http_method_names=['delete'])),
+    path('my_companies/', CustomUserCompaniesAPIView.as_view(http_method_names=['get'])),
 
     path('project/', ProjectAPIView.as_view(http_method_names=['post', 'options'])),
     path('project/<int:project_id>', ProjectAPIView.as_view(http_method_names=['get', 'put', 'delete'])),
