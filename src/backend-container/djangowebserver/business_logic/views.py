f5f6a7c5332fcb7f1b74e5e8fa0d3cddeebfe5c0
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 16:34:01 +0200

    implemented DELETE for company/

--- src/backend-container/djangowebserver/business_logic/views.py
+++ src/backend-container/djangowebserver/business_logic/views.py
@@ -36,7 +36,7 @@ class CompanyAPIView(GenericAPIView):
             try:
                 token = request.headers['Authorization'].split()[1]
                 user_id = AccessToken(token=token)['user_id']
-                assert request.data['creator_id'] == user_id
+                assert request.data['creator'] == user_id
             except AssertionError:
                 return Response({'message': 'Permission denied.'}, status=status.HTTP_403_FORBIDDEN)
 
@@ -49,3 +49,25 @@ class CompanyAPIView(GenericAPIView):
                 status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @permission_classes([IsAuthenticated])
+    def delete(self, request, id):
+        try:
+            company = Company.objects.get(pk=id)
+        except:
+            return Response({'message': 'Company not found.'}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            token = request.headers['Authorization'].split()[1]
+            user_id = AccessToken(token=token)['user_id']
+            assert user_id == company.creator.id     # if user that invokes deletion is a creator of a company
+        except AssertionError:
+            return Response({'message': 'Permission denied.'}, status=status.HTTP_403_FORBIDDEN)
+        name = company.name
+        description = company.description
+        creator_id = company.creator.id
+        company.delete()
+        return Response(
+            {
+                'message': f'Company with id {id} (name: {name}, description: {description}, creator_id: {creator_id}) has been successfully deleted.',
+            },
+            status=status.HTTP_200_OK)
