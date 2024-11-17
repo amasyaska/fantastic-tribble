69acc3a1cb2df6c0de94042712a195551bcc52ab
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 20:35:51 +0200

    implemented add members to companies

--- src/backend-container/djangowebserver/business_logic/urls.py
+++ src/backend-container/djangowebserver/business_logic/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import CompanyAPIView
+from .views import CompanyAPIView, CompanyMembersAPIView
 
 urlpatterns = [
     path('company/', CompanyAPIView.as_view(http_method_names=['post', 'options'])),
     path('company/<int:id>', CompanyAPIView.as_view(http_method_names=['get', 'put', 'delete'])),
+    path('company/<int:company_id>/members/', CompanyMembersAPIView.as_view(http_method_names=['get', 'post', 'put'])),
+    path('company/<int:company_id>/members/<int:member_id>', CompanyMembersAPIView.as_view(http_method_names=['delete'])),
 ]
