3f3bbe5695db00cedd6b5fe2869a4e62e8b93246
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-17 13:39:36 +0200

    implemented GET by id for user/ endpoint

--- src/backend-container/djangowebserver/accounts/views.py
+++ src/backend-container/djangowebserver/accounts/views.py
@@ -7,10 +7,25 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import CustomUserCreateSerializer, LoginSerializer
+from .models import CustomUser
 
 class UserAPIView(GenericAPIView):
 
     serializer_class = CustomUserCreateSerializer
+
+    def get(self, request, id):
+        try:
+            user = CustomUser.objects.get(pk=id)
+        except:
+            return Response({'message': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {
+                'id': user.id,
+                'username': user.username,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+            },
+            status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -19,7 +34,7 @@ class UserAPIView(GenericAPIView):
             serializer.save()
             return Response(
                 {
-                'message': 'created user successfully',
+                'message': 'Created user successfully.',
                 'user_data': serializer.data
                 },
                 status=status.HTTP_201_CREATED)
